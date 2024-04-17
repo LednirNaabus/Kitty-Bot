b49da52454de60691d254eb88b03a403f474e15e
Rindel Subaan <valsubaan@yahoo.com>
2024-04-18 00:59:35 +0800

    Modified Pics class; Added to do comments

--- kittybot/cogs/pics.py
+++ kittybot/cogs/pics.py
@@ -1,7 +1,8 @@
 """
 === Cat Pics command for Kitty Bot
 """
-from config.supabase_config import SupabaseClient
+# from config.supabase_config import SupabaseClient
+from kittybot.bot import KittyBot
 from shared.common import *
 
 """Sends cat memes or pics to the user using the `pics` command"""
@@ -9,10 +10,16 @@ class Pic(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
+    """
+    TODO:
+    - Do something about SupabaseClient
+    - Dapat yung url ng image random urls from the database
+    """
+
     @commands.command(name='sendpic', aliases=['sp'])
     async def pic_embed(self, ctx):
         embed = discord.Embed(
-            title='cat pic!',
+            title='random cat pic!',
             description='Lorem ipsum'
         )
         embed.set_author(name='Kitty Bot')
@@ -20,7 +27,7 @@ class Pic(commands.Cog):
         embed.add_field(name='here\'s a cat pic for you! *meow*', value=ctx.author.mention)
         embed.set_footer(text='Image retrieved from Supabase. Please support Kitty Bot by following the bot author\'s Github page.')
 
-        await ctx.send(content='Embed', embed=embed)
+        await ctx.send(embed=embed)
 
 async def setup(bot) -> None:
     await bot.add_cog(Pic(bot))
