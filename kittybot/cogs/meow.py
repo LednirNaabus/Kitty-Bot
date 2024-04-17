41eb1040cb065529ea088f074dd4b1dd57d9e057
Rindel Subaan <valsubaan@yahoo.com>
2024-04-17 23:23:48 +0800

    Make 'meow' lowercase

--- kittybot/cogs/meow.py
+++ kittybot/cogs/meow.py
@@ -21,31 +21,31 @@ class Meow(commands.Cog):
             "cute_face": "\U0001F97A",
             "sleepy_face": "\U0001F634"
         }
-        
+
     # Happy meow
     @commands.command(name='happymeow', aliases=['mh'])
     async def happy_meow(self, ctx):
-        await ctx.send(f'Meow! *purr*... {ctx.author} {self.emotion["happy_face"]}')
+        await ctx.send(f'meow! *purr*... {ctx.author} {self.emotion["happy_face"]}')
 
     # Sad meow
     @commands.command(name='sadmeow', aliases=['ms'])
     async def sad_meow(self, ctx):
-        await ctx.send(f'Meow... {ctx.author} {self.emotion["sad_face"]}')
+        await ctx.send(f'meow... {ctx.author} {self.emotion["sad_face"]}')
 
     # Angry meow
     @commands.command(name='hiss', aliases=['ma'])
     async def angry_meow(self, ctx):
-        await ctx.send(f'Meow!!! *hiss* {ctx.author} {self.emotion["angry_face"]}')
+        await ctx.send(f'meow!!! *hiss* {ctx.author} {self.emotion["angry_face"]}')
 
     # Cute Meow
     @commands.command(name='cutemeow', aliases=['mc'])
     async def cute_meow(self, ctx):
-        await ctx.send(f'Meow! uwu {ctx.author} {self.emotion["cute_face"]}')
+        await ctx.send(f'meow! uwu {ctx.author} {self.emotion["cute_face"]}')
 
     # Sleepy Meow
     @commands.command(name='sleepymeow', aliases=['mzzz'])
     async def sleepy_meow(self, ctx):
-        await ctx.send(f'Meow... zzz {ctx.author} {self.emotion["sleepy_face"]}')
+        await ctx.send(f'meow... zzz {ctx.author} {self.emotion["sleepy_face"]}')
 
 async def setup(bot) -> None:
     await bot.add_cog(Meow(bot))
