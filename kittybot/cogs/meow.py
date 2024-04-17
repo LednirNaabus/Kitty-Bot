d2b885476e470a5b56827fd5e336e0a461e842f4
Rindel Subaan <valsubaan@yahoo.com>
2024-04-17 19:05:45 +0800

    Refactored Meow commands

--- kittybot/cogs/meow.py
+++ kittybot/cogs/meow.py
@@ -14,68 +14,38 @@ import asyncio
 class Meow(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        self._last_member = None
-
+        self.emotion = {
+            "happy_face": "\U0001F63A",
+            "sad_face": "\U0001F63F",
+            "angry_face": "\U0001F63E",
+            "cute_face": "\U0001F97A",
+            "sleepy_face": "\U0001F634"
+        }
+        
     # Happy meow
-    @commands.command(aliases=['mh'])
-    async def happy_meow(self, ctx, *, member: discord.Member = None):
-        # args = ctx.message.content.split()
-        member = member or ctx.author
-
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send(f'Meow. {member.name}~~ *purr*')
-        else:
-            await ctx.send(f'Meow! {member.name} :)')
-        self._last_member = member
+    @commands.command(name='happymeow', aliases=['mh'])
+    async def happy_meow(self, ctx):
+        await ctx.send(f'Meow! *purr*... {ctx.author} {self.emotion["happy_face"]}')
 
     # Sad meow
-    @commands.command(aliases=['ms'])
-    async def sad_meow(self, ctx, *, member: discord.Member = None):
-        # args = ctx.message.content.split()
-        member = member or ctx.author
-
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send(f'Meow. {member.name}~~ *purr*')
-        else:
-            await ctx.send(f'Meow... {member.name} :(')
-        self._last_member = member
+    @commands.command(name='sadmeow', aliases=['ms'])
+    async def sad_meow(self, ctx):
+        await ctx.send(f'Meow... {ctx.author} {self.emotion["sad_face"]}')
 
     # Angry meow
-    @commands.command(aliases=['ma'])
-    async def angry_meow(self, ctx, *, member: discord.Member = None):
-        # args = ctx.message.content.split()
-        member = member or ctx.author
+    @commands.command(name='hiss', aliases=['ma'])
+    async def angry_meow(self, ctx):
+        await ctx.send(f'Meow!!! *hiss* {ctx.author} {self.emotion["angry_face"]}')
 
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send(f'Meow. {member.name}~~ *purr*')
-        else:
-            await ctx.send(f'Meow!!! *hiss* {member.name} >:(')
-        self._last_member = member
+    # Cute Meow
+    @commands.command(name='cutemeow', aliases=['mc'])
+    async def cute_meow(self, ctx):
+        await ctx.send(f'Meow! uwu {ctx.author} {self.emotion["cute_face"]}')
 
-    # Cute meow
-    @commands.command(aliases=['mc'])
-    async def cute_meow(self, ctx, *, member: discord.Member = None):
-        # args = ctx.message.content.split()
-        member = member or ctx.author
-
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send(f'Meow. {member.name}~~ *purr*')
-        else:
-            await ctx.send(f'Meow! :3 {member.name} uwu')
-        self._last_member = member
-
-    # Sleepy meow
-    @commands.command(aliases=['mzzz'])
-    async def sleepy_meow(self, ctx, *, member: discord.Member = None):
-        # args = ctx.message.content.split()
-        member = member or ctx.author
-
-        if self._last_member is None or self._last_member.id != member.id:
-            await ctx.send(f'Meow. {member.name}~~ *purr*')
-        else:
-            await ctx.send(f'Meow... {member.name} zzz')
-        self._last_member = member
-
+    # Sleepy Meow
+    @commands.command(name='sleepymeow', aliases=['mzzz'])
+    async def sleepy_meow(self, ctx):
+        await ctx.send(f'Meow... zzz {ctx.author} {self.emotion["sleepy_face"]}')
 
 async def setup(bot) -> None:
     await bot.add_cog(Meow(bot))
