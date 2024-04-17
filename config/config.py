c004fbebf380fbe27cafd3a6d26185a302e61636
Rindel Subaan <valsubaan@yahoo.com>
2024-04-17 20:42:44 +0800

    Added Supabase configs; Added project dependencies and requirements

--- config/config.py
+++ config/config.py
@@ -1,9 +1,15 @@
 import os
 from dotenv import load_dotenv
+from supabase import create_client, Client
 
 load_dotenv()
 
+# ENVIRONMENT VARIABLES
 DISCORD_BOT_TOKEN = os.environ["BOT_TOKEN"]
+# Supabase configs
+url: str = os.environ["SUPABASE_PROJ_URL"]
+key: str = os.environ["SUPABASE_API_KEY"]
+supabase: Client = create_client(url, key)
 
 # Bot configs
 STARTUP_MESSAGE = "Starting Kitty Bot v.1..."
